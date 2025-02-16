16bc142f48a2a3bb2c8313d8751c853b0a7f364e
vohopvang <Vohopvang09@gmail.com>
2025-02-16 15:58:39 +0700

    Add files via upload

--- Vangscript.lua
+++ Vangscript.lua
@@ -3,7 +3,7 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
        local Window = MakeWindow({
          Hub = {
          Title = "Vàng Script",
-         Animation = "Các bạn chơi game vui vẻ"
+         Animation = "Chơi game vui vẻ"
          },
         Key = {
         KeySystem = false,
@@ -22,8 +22,8 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
 
        MinimizeButton({
        Image = "http://www.roblox.com/asset/?id=119141649014792 ",
-       Size = {60, 60},
-       Color = Color3.fromRGB(10, 10, 10),
+       Size = {50, 50},
+       Color = Color3.fromRGB(245, 10, 10),
        Corner = true,
        Stroke = false,
        StrokeColor = Color3.fromRGB(255, 0, 0)
@@ -45,3 +45,35 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
 loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))(Settings)
   end
   })
+      AddButton(Tab2o, {
+     Name = "W Azure",
+    Callback = function()
+	 getgenv().Team = "Pirates"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
+  end
+  })
+      AddButton(Tab3o, {
+     Name = "Xero Hub",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/verudous/Xero-Hub/main/main.lua"))()
+  end
+  })
+      AddButton(Tab4o, {
+     Name = "Speed Hub",
+    Callback = function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+  end
+  })
+      AddButton(Tab5o, {
+     Name = "Trẩu Roblox",
+    Callback = function()
+	 loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/TrauHub/refs/heads/main/TrauXt"))()  
+  end
+  })
+  AddButton(Tab6o, {
+     Name = "Min Gaming",
+    Callback = function()
+	 loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/Min/refs/heads/main/MinCE"))() 
+  end
+  })
+  
